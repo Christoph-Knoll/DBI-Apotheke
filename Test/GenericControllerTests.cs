51b9e1fe768ecf403e3abded45afc38eeb70a4d2
Sandro6880 <31956997+Sandro6880@users.noreply.github.com>
2021-01-16 23:11:43 +0100

    Update GenericControllerTests

--- Test/GenericControllerTests.cs
+++ Test/GenericControllerTests.cs
@@ -28,6 +28,7 @@ namespace MongoDBDemoApp.Test
                 Unit = DBI_Apotheke.Core.Workloads.Modules.Unit.G
             };
             var list = new List<Ingredient>();
+            list.Add(ingredient);
             var productInfo1 = new ProductInfo
             {
                 Brand = "Aspiring",
@@ -36,14 +37,19 @@ namespace MongoDBDemoApp.Test
                 Name = "AspirinComplex"
             };
             var repoMock = Substitute.For<IGenericRepository<ProductInfo>>();
-
+            repoMock.GetItemById(Arg.Any<ObjectId>()).Returns(ci => new ProductInfo
+            {
+                Id = ci.Arg<ObjectId>()
+            });
             await repoMock.InsertItem(productInfo1);
 
-            var result = repoMock.GetItemById(Arg.Any<ObjectId>()).Returns(pi => new ProductInfo
-            {
-                Id = pi.Arg<ObjectId>()
-            });
-            result.Should().BeSameAs(productInfo1);
+            await repoMock.Received(1).InsertItem(Arg.Any<ProductInfo>());
+            
+            var result = await repoMock.GetItemById(productInfo1.Id);
+            var result2 = await repoMock.GetAllItems();
+            //await repoMock.Received(1).GetItemById(productInfo1.Id);
+
+            result.Should().NotBeNull();
         }
     }
 }
