6133e8bad1722be8b2b2ba1a5678f0b0066190aa
AlexK2001 <kirchmairalex2001@gmail.com>
2021-01-16 21:30:37 +0100

    add model structure

---
+++ App/Model/Recipe/RecipeDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBI_Apotheke.Model.Recipe
+{
+    public class RecipeDTO
+    {
+    }
+}
