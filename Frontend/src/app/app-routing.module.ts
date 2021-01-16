eb4dd3604ccab9fd560e0425f61d5f2f92bdf97d
Elias Röbl <eliasroebl@gmail.com>
2021-01-17 00:13:09 +0100

    added Route to RedeemRecipeComponent

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {AddRecipeComponent} from './add-recipe/add-recipe.component';
+import {RedeemRecipeComponent} from './redeem-recipe/redeem-recipe.component';
 
 const routes: Routes = [
-  { path: 'addRecipe', component: AddRecipeComponent }
+  { path: 'addRecipe', component: AddRecipeComponent },
+  { path: 'redeemRecipe', component: RedeemRecipeComponent}
 ];
 
 @NgModule({
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import {RouterModule} from '@angular/router';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RedeemRecipeComponent } from './redeem-recipe/redeem-recipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddRecipeComponent
+    AddRecipeComponent,
+    RedeemRecipeComponent
   ],
     imports: [
         BrowserModule,
