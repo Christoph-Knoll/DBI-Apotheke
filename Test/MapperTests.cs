32cb67a66eecbb5985204a576b2b03330a0113e4
Sandro6880 <31956997+Sandro6880@users.noreply.github.com>
2021-01-16 22:10:32 +0100

    add MapperTests

---
+++ Test/MapperTests.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DBI_Apotheke.Core.Util;
+using Xunit;
+
+namespace MongoDBDemoApp.Test
+{
+    public sealed class MapperTests
+    {
+        [Fact]
+        public void TestMappingProfile()
+        {
+            var profile = new MapperProfile();
+            var cfg = new MapperConfiguration(c => c.AddProfile(profile));
+            cfg.AssertConfigurationIsValid();
+        }
+    }
+}
