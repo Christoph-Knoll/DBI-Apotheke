a60997059ce8dfdab71e95aaecacd9273a1c697c
Elias Röbl <eliasroebl@gmail.com>
2021-01-17 01:29:48 +0100

    added Product Service

---
+++ Frontend/src/app/services/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {GenericHttpService} from './GenericHttpService/GenericHttpService';
+import {IProduct} from '../contracts/IProduct';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService extends GenericHttpService<IProduct, number> {
+
+  constructor(protected http: HttpClient) {
+    // #TODO Add path
+    super(http, `${environment.api.baseUrl}/`);
+  }
+}
