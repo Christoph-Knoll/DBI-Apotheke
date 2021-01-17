a7bab84f41d5b726cb6c80801c02934be1fcb7a6
Christoph Knoll <c.knoll@htblaleonding.ac.at>
2021-01-17 15:59:54 +0100

    Add ServiceBase add StorageService

---
+++ App/Core/Workloads/Storages/IStorageService.cs
@@ -0,0 +1,13 @@
+﻿using DBI_Apotheke.Core.Workloads.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBI_Apotheke.Core.Workloads.Storages
+{
+    interface IStorageService: IServiceBase<Storage>
+    {
+        Task<Storage> InsertItem(int pzn, int amount, string storageSite);
+    }
+}
---
+++ App/Core/Workloads/Storages/StorageService.cs
@@ -0,0 +1,34 @@
+﻿using DBI_Apotheke.Core.Util;
+using DBI_Apotheke.Core.Workloads.Generics;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBI_Apotheke.Core.Workloads.Storages
+{
+    public class StorageService : ServiceBase<Storage>, IStorageService
+    {
+        private readonly IStorageRepository _repository;
+
+
+        public StorageService(IDateTimeProvider dateTimeProvider, IStorageRepository repository)
+            :base(dateTimeProvider, repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Storage> InsertItem(int pzn, int amount, string storageSite)
+        {
+            var storage = new Storage
+            {
+                PZN = pzn,
+                Amount = amount,
+                StorageSite = storageSite
+            };
+
+            return this._repository.InsertItem(storage);
+        }
+    }
+}
