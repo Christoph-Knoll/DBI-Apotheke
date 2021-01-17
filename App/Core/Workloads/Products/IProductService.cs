34ca36137bac60fcaef27e8a7dac29853ad3b5e3
Christoph Knoll <c.knoll@htblaleonding.ac.at>
2021-01-17 16:19:08 +0100

    Add product service

--- App/Core/Workloads/Products/IProductService.cs
+++ App/Core/Workloads/Products/IProductService.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using DBI_Apotheke.Core.Workloads.Generics;
+using DBI_Apotheke.Core.Workloads.Modules;
+using DBI_Apotheke.Core.Workloads.ProductInfos;
+using DBI_Apotheke.Core.Workloads.Storages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DBI_Apotheke.Core.Workloads.Products
 {
-    interface IProductService
+    interface IProductService : IServiceBase<Product>
     {
+        Task<Product> InsertItem(ProductInfo productInfo, int pzn, double price, int amount, Unit unit);
+
     }
 }
