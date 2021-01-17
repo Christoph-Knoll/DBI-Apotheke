201217ad46291fe784d37f91427a79061420bde0
Elias Röbl <eliasroebl@gmail.com>
2021-01-17 01:30:05 +0100

    added Recipe Service

---
+++ Frontend/src/app/services/recipe.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {GenericHttpService} from './GenericHttpService/GenericHttpService';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {IRecipe} from '../contracts/IRecipe';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService extends GenericHttpService<IRecipe, number> {
+
+  constructor(protected http: HttpClient) {
+    // #TODO Add path
+    super(http, `${environment.api.baseUrl}/`);
+  }
+}
