6ce922bc84406579cd199907857aab8cd1fe39eb
Elias Röbl <eliasroebl@gmail.com>
2021-01-17 20:22:22 +0100

    changed name and added table

--- Frontend/src/app/prdoucts/products.component.spec.ts
+++ Frontend/src/app/prdoucts/products.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PrdouctsComponent } from './prdoucts.component';
+import { ProductsComponent } from './products.component';
 
 describe('PrdouctsComponent', () => {
-  let component: PrdouctsComponent;
-  let fixture: ComponentFixture<PrdouctsComponent>;
+  let component: ProductsComponent;
+  let fixture: ComponentFixture<ProductsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PrdouctsComponent ]
+      declarations: [ ProductsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PrdouctsComponent);
+    fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
