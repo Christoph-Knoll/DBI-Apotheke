3032e340d2e4e3327d6e65b0af67507c10bfb240
Elias Röbl <eliasroebl@gmail.com>
2021-01-17 01:30:16 +0100

    added Storage Service

---
+++ Frontend/src/app/services/storage.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {GenericHttpService} from './GenericHttpService/GenericHttpService';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {IStorage} from '../contracts/IStorage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService extends GenericHttpService<IStorage, number> {
+
+  constructor(protected http: HttpClient) {
+    // #TODO Add path
+    super(http, `${environment.api.baseUrl}/`);
+  }
+}
