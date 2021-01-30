0a057f7d4314416fd2c7caa1b6d4e1f5a6c0e310
AlexK2001 <kirchmairalex2001@gmail.com>
2021-01-30 17:22:10 +0100

    add GetAllProductsByProductInfo Method

--- App/Core/Workloads/ProductInfos/IProductInfoRepository.cs
+++ App/Core/Workloads/ProductInfos/IProductInfoRepository.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson;
 using DBI_Apotheke.Core.Workloads.Generics;
+using DBI_Apotheke.Core.Workloads.Products;
 
 namespace DBI_Apotheke.Core.Workloads.ProductInfos
 {
     public interface IProductInfoRepository : IGenericRepository<ProductInfo>
     {
         Task<IReadOnlyCollection<ProductInfo>> GetByIngredient(string ingredientName);
+
     }
 }
