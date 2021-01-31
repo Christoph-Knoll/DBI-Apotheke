173f1b5da39b411e305fe845d29716cc339d35d2
Elias Röbl <eliasroebl@gmail.com>
2021-01-31 17:41:53 +0100

    changed id type

--- Frontend/src/app/services/product-info.service.ts
+++ Frontend/src/app/services/product-info.service.ts
@@ -7,7 +7,7 @@ import {IProductInfo} from '../contracts/IProductInfo';
 @Injectable({
   providedIn: 'root'
 })
-export class ProductInfoService extends GenericHttpService<IProductInfo, number> {
+export class ProductInfoService extends GenericHttpService<IProductInfo, string> {
 
   constructor(protected http: HttpClient) {
     super(http, `${environment.api.baseUrl}/productInfo`);
